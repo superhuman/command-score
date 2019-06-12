a4204edd27014cce4c879925225722894da702da
hehk <kyle1101000@gmail.com>
2019-06-12 11:09:51 -0700

    format input so all 'space' characters become the same ' ' char

--- index.js
+++ index.js
@@ -127,12 +127,17 @@ function commandScoreInner(string, abbreviation, lowerString, lowerAbbreviation,
     return highScore;
 }
 
+function formatInput (string) {
+  // convert all valid space characters to space so they match each other
+  return string.toLowerCase().replace(COUNT_SPACE_REGEXP, ' ')
+}
+
 function commandScore(string, abbreviation) {
     /* NOTE:
      * in the original, we used to do the lower-casing on each recursive call, but this meant that toLowerCase()
      * was the dominating cost in the algorithm, passing both is a little ugly, but considerably faster.
      */
-    return commandScoreInner(string, abbreviation, string.toLowerCase(), abbreviation.toLowerCase(), 0, 0);
+    return commandScoreInner(string, abbreviation, formatInput(string), formatInput(abbreviation), 0, 0);
 }
 
 module.exports = commandScore;
